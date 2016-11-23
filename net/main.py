93db88f9caa3f6073150d944da8bd97116339143
Bruno <brunosmarsaro@gmail.com>
2016-11-23 13:23:55 -0200

    input ok

--- net/main.py
+++ net/main.py
@@ -22,7 +22,7 @@ if nw.inputs == -1: exit()
 max_steps = 10000
 batch_size = 50
 
-print("First Convolutional Layer")
+# First Convolutional Layer
 
 W_conv1 = nw.weight_variable([55, 55, 5, 96])
 b_conv1 = nw.bias_variable([96])
@@ -32,7 +32,7 @@ x_image = tf.reshape(nw.x, [-1,1024,1024,5])
 h_conv1 = tf.nn.relu(nw.conv2d(x_image, W_conv1) + b_conv1)
 h_pool1 = nw.max_pool_2x2(h_conv1)
 
-print("Second Convolutional Layer")
+# Second Convolutional Layer
 
 W_conv2 = nw.weight_variable([27, 27, 96, 256])
 b_conv2 = nw.bias_variable([256])
@@ -40,7 +40,7 @@ b_conv2 = nw.bias_variable([256])
 h_conv2 = tf.nn.relu(nw.conv2d(h_pool1, W_conv2) + b_conv2)
 h_pool2 = nw.max_pool_2x2(h_conv2)
 
-print("Third Convolutional Layer")
+# Third Convolutional Layer
 
 W_conv3 = nw.weight_variable([13, 13, 256, 384])
 b_conv3 = nw.bias_variable([384])
@@ -48,14 +48,15 @@ b_conv3 = nw.bias_variable([384])
 h_conv3 = tf.nn.relu(nw.conv2d(h_pool2, W_conv3) + b_conv3)
 h_pool3 = nw.max_pool_2x2(h_conv3)
 
-print("Fourth Convolutional Layer")
+# Fourth Convolutional Layer
+
 W_conv4 = nw.weight_variable([13, 13, 384, 384])
 b_conv4 = nw.bias_variable([384])
 
 h_conv4 = tf.nn.relu(nw.conv2d(h_pool3, W_conv4) + b_conv4)
 h_pool4 = nw.max_pool_2x2(h_conv4)
 
-print("Fifth Convolutional Layer")
+# Fifth Convolutional Layer
 
 W_conv5 = nw.weight_variable([13, 13, 384, 256])
 b_conv5 = nw.bias_variable([256])
@@ -63,7 +64,7 @@ b_conv5 = nw.bias_variable([256])
 h_conv5 = tf.nn.relu(nw.conv2d(h_pool4, W_conv5) + b_conv5)
 h_pool5 = nw.max_pool_2x2(h_conv4)
 
-print("First Fully Connected Layer")
+# First Fully Connected Layer
 
 W_fc1 = nw.weight_variable([7 * 7 * 64, 4096])
 b_fc1 = nw.bias_variable([4096])
@@ -71,7 +72,7 @@ b_fc1 = nw.bias_variable([4096])
 h_pool1_flat = tf.reshape(h_pool5, [-1, 7*7*64])
 h_fc1 = tf.nn.relu(tf.matmul(h_pool1_flat, W_fc1) + b_fc1)
 
-print("Second Fully Connected Layer")
+# Second Fully Connected Layer
 
 W_fc2 = nw.weight_variable([7 * 7 * 64, 4096])
 b_fc2 = nw.bias_variable([4096])
@@ -79,7 +80,7 @@ b_fc2 = nw.bias_variable([4096])
 h_pool2_flat = tf.reshape(h_pool1_flat, [-1, 7*7*64])
 h_fc2 = tf.nn.relu(tf.matmul(h_pool2_flat, W_fc2) + b_fc2)
 
-print("Third Fully Connected Layer (Readout Layer)")
+# Third Fully Connected Layer (Readout Layer)
 
 keep_prob = tf.placeholder(tf.float32)
 h_fc2_drop = tf.nn.dropout(h_fc2, keep_prob)
@@ -89,16 +90,12 @@ b_fc3 = nw.bias_variable([2])
 
 y_conv = tf.nn.softmax(tf.matmul(h_fc2_drop, W_fc3) + b_fc3)
 
-print("Preparing the Model")
+# Train and Evaluate the Model
 
 cross_entropy = tf.reduce_mean(-tf.reduce_sum(nw.y_ * tf.log(y_conv), reduction_indices=[1]))
-print("1")
 train_step = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy)
-print("2")
 correct_prediction = tf.equal(tf.argmax(y_conv,1), tf.argmax(nw.y_,1))
-print("3")
 accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
-print("4")
 sess.run(tf.initialize_all_variables())
 
 print("Train and Evaluate the Model")
