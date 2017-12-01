e361cf809977b404711c4b9fc7483f431ee0a7b0
Bruno Smarsaro Bazelato <brunosmarsaro@gmail.com>
2017-11-30 22:18:51 -0200

    Mias database to file

--- csvimg.py
+++ csvimg.py
@@ -2,15 +2,16 @@ import numpy as np
 from scipy import misc
 import csv
 from matplotlib import pyplot as plt
+import os
 
-image_path = "./MIAS/malignant/mdb141.pgm"
-img = misc.imread(image_path, mode='L', flatten=True)
-#plt.imshow(img)
-#plt.show()
-img = np.array(img)
-
-np.savetxt('img_array.out', img, fmt='%f', delimiter=',')
-'''
-for line in img:
-	print(line)
-'''
+_dir = ".\database\ALL_MIAS"
+for s,r,f in os.walk(_dir):
+	for file in f:
+		name, ext = os.path.splitext(file)
+		if not ("pgm" in ext): continue
+		image_path = os.path.join(s, file)
+		print(image_path)
+		img = misc.imread(image_path, mode='L', flatten=True)
+		img = np.array(img)
+		np.savetxt('%s\%s.txt'%(_dir, name), img, fmt='%f', delimiter=',')
+		
